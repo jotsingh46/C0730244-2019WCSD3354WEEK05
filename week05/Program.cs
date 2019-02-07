ed7740ac7c1162525cb6c98c9ec04528f3c9cb66
visualstudio <visualstudio@230-PC25>
2019-02-07 14:10:01 -0500

    2:05COMMIT

--- week05/Program.cs
+++ week05/Program.cs
@@ -6,7 +6,36 @@ namespace week05
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
+
+
+    class TestQuestion2
+    {
+        public int myFavouriteVariable = 0;
+        public void PlayingWithForLoops()
+        {
+            for( ; MyMethod() ; )
+            {
+                if (myFavouriteVariable > 10)
+                {
+                    Console.WriteLine("i am so out of here");
+                    
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("oh no i have to go through this again");
+                    
+                }
+            }
+        }
+        public bool MyMethod()
+        {
+            myFavouriteVariable++;
+            return true;
+        }
+    }
+
 }
